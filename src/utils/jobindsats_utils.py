aaaa84b0470671a1590f1fdd541867be3c9e3d8a
nk-randers <nivethan.kandiah@randers.dk>
2025-09-02 10:13:20 +0200

    added utils for selectbox options

---
+++ src/utils/jobindsats_utils.py
@@ -0,0 +1,11 @@
+udfaldsmål_options = [
+        "Forventet andel (pct.)",
+        "Faktisk andel (pct.)"
+    ]
+
+målgruppe_options = [
+        "Sygedagpenge mv.",
+        "Ydelsesgrupper i alt",
+        "A-dagpenge mv.",
+        "Kontanthjælp mv."
+    ]
