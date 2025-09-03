c47534a8c0011cbc0f4dbd568714c6485e5947c0
nk-randers <nivethan.kandiah@randers.dk>
2025-09-03 13:06:04 +0200

    Update utils

--- src/utils/jobindsats_utils.py
+++ src/utils/jobindsats_utils.py
@@ -1,11 +1,11 @@
-udfaldsmål_options = [
-        "Forventet andel (pct.)",
-        "Faktisk andel (pct.)"
-    ]
+ydelsesgrupper_udfaldsmål_options = [
+    "Forventet andel (pct.)",
+    "Faktisk andel (pct.)",
+]
 
-målgruppe_options = [
-        "Sygedagpenge mv.",
-        "Ydelsesgrupper i alt",
-        "A-dagpenge mv.",
-        "Kontanthjælp mv."
-    ]
+ydelsesgrupper_målgruppe_options = [
+    "Sygedagpenge mv.",
+    "Ydelsesgrupper i alt",
+    "A-dagpenge mv.",
+    "Kontanthjælp mv."
+]
