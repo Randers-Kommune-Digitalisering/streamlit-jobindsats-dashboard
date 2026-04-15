8d83bf9fb3d7824d5b78ff8dd0b859ffc64f28e7
Karl Fritjof Krassel <kfkr@randers.dk>
2026-04-15 09:53:44 +0200

    Udvidelse med forside

---
+++ src/page/forside.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def show_forside():
+    st.title("Jobindsats-dashboard")
+    st.markdown("## Velkommen til Randers Kommunes jobindsats-dashboardet")
+    st.markdown("Data trækkes fra [api.jobindsats.dk](https://api.jobindsats.dk/) og opdateres ugentlig. Formålet med dashboardet er at give et overblik over jobindsatsen i Randers Kommune og gøre det nemt for medarbejdere, ledere og beslutningstagere at få indsigt i relevante data.")
+    st.markdown("Dashboardet er opdelt i flere sektioner, som hver især fokuserer på forskellige aspekter af jobindsatsen. I menuen til venstre kan du navigere mellem de forskellige sektioner og udforske de data og grafer, der er tilgængelige.")
+    st.markdown("Data på arbejdsamarkedsområdet fra kommunes KMD Insight-løsning (BI) kan tilgås via [KMD Insight](https://ssolaunchpad.kmd.dk/SSO/Redirect?https://kmd-opus-ledelsesinformation.kmd.dk/BOE/OpenDocument/opendoc/openDocument.jsp?sIDType=CUID&iDocID=AZk._LD5leVIvvXtHie8kM0&sViewer=fiori&kommune=730) " )
+    st.markdown("""
+        ## Indhold
+  
+        | Menupunkt         | Beskrivelse
+        |---------------------------|-------------------------------------|
+        | ***JobRanders***     | Måltal for centre og afdelinger i JobRanders (under udvikling) | 
+        | ***Ydelser***          | Udvikling i enkeltydelser over tid (under udvikling) | 
+        | ***Fremtidens Randers***    | Diverse grafer i relation til Fremtidens Randers (under udvikling) |
+        | ***Datakatalog***                | Katalog over tilgængelige data fra api.jobindsats.dk  | 
+        | ***Om***          | Om denne side | 
+    """)
