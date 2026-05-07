24e506bde97eefbb1f539adfa71c36097502e60b
Karl Fritjof Krassel <kfkr@randers.dk>
2026-05-07 15:58:33 +0200

    formattering

--- src/graphs/cjk.py
+++ src/graphs/cjk.py
@@ -3,6 +3,7 @@ import altair as alt
 import pandas as pd
 import plotly.express as px
 from utils.database_connection import get_jobindsats_db
+from matplotlib.ticker import FuncFormatter
 
 db_client = get_jobindsats_db()
 
@@ -10,19 +11,24 @@ def date_parser(df, date_column):
     df[date_column] = pd.to_datetime(df[date_column].str.replace('M', '-'), format='%Y-%m')
     return df
 
+def percent_comma(x, pos):
+    return f"{x:,.1f}%".replace('.', ',')
 
 def cjk_page():
 
     st.text("Overordnede mål")
     st.text("1. Sæsonkorrigeret ledighedsudvikling")
 
-    querytest = (
-        'SELECT * from jobindsats_y25i01'
-    )
-    resulttest = db_client.execute_sql(querytest)
-    dftest = pd.DataFrame(resulttest)
+    # querytest = (
+    #     'SELECT * from jobindsats_y25i01'
+    # )
+    # resulttest = db_client.execute_sql(querytest)
+    # dftest = pd.DataFrame(resulttest)
 
-    st.write(dftest)
+    # st.write(dftest)
+
+    # i dag
+    today = pd.to_datetime("today")
 
     query = (
         'SELECT "Område", "Periode", "Antal ledige personer", "Ledige fuldtidspersoner i pct. af arbejdsstyrken 16-66 år", "Ledige fuldtidspersoner i pct. af befolkningen16-66 år" FROM jobindsats_y25i01 where "Område" IN (\'Randers\', \'Aarhus\', \'Favrskov\', \'Horsens\', \'Norddjurs\', \'Odder\', \'Randers\', \'Samsø\',  \'Skanderborg\',  \'Syddjurs\') order by "Periode" desc;'
@@ -32,7 +38,7 @@ def cjk_page():
 
     df = pd.DataFrame(result, columns=["Område", "Periode", "Antal ledige personer", "Ledige fuldtidspersoner i pct. af arbejdsstyrken 16-66 år", "Ledige fuldtidspersoner i pct. af befolkningen16-66 år"])
 
-    st.write(df)
+    # st.write(df)
 
     chart_df = df[["Område", "Periode", "Ledige fuldtidspersoner i pct. af arbejdsstyrken 16-66 år"]]
     chart_df["Ledige fuldtidspersoner i pct. af arbejdsstyrken 16-66 år"] = pd.to_numeric(chart_df["Ledige fuldtidspersoner i pct. af arbejdsstyrken 16-66 år"], errors='coerce')
@@ -42,6 +48,7 @@ def cjk_page():
     chart_df["År"] = chart_df["Periode"].dt.year
     chart_df["Måned"] = chart_df["Periode"].dt.month
 
+    chart_df = chart_df[chart_df["År"] >= today.year - 2 ]
 
     # st.write(chart_df)
 
@@ -61,15 +68,18 @@ def cjk_page():
 
     import matplotlib.pyplot as plt
     # Pyplot chart for the same data
-    fig, ax = plt.subplots(figsize=(6, 4))
+    fig, ax = plt.subplots(figsize=(8, 4))
+    colors = {'Randers': '#00B050', 'Østjylland': '#FFC000'}
     for område, group in grouped_df.groupby("Område_split"):
-        ax.plot(group['Periode'], group['Ledige fuldtidspersoner i pct. af arbejdsstyrken 16-66 år'], label=område)
-
+        ax.plot(group['Periode'], group['Ledige fuldtidspersoner i pct. af arbejdsstyrken 16-66 år'], label=område, color=colors.get(område, 'black'))
     ax.set_xlabel('Tid')
     ax.set_ylabel('Procent')
     ax.set_title('Sæsonkorrigeret ledighed i procent af arbejdsstyrken 16-66 år')
-    ax.grid(True)
-    ax.legend(title="Område", loc='center left', bbox_to_anchor=(1, 0.5))
+    ax.grid(axis='y', color='gray', linestyle='-', linewidth=0.5, alpha=0.2)
+    ax.yaxis.set_major_formatter(FuncFormatter(percent_comma))
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.5), frameon=False)
     fig.autofmt_xdate()
     st.pyplot(fig, use_container_width=False)
     
