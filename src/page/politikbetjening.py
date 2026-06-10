35ba3637917107735533e171c12ef218b0a3e3d2
Karl Fritjof Krassel <kfkr@randers.dk>
2026-06-10 13:51:21 +0200

    Politisk betjening

--- src/page/politikbetjening.py
+++ src/page/politikbetjening.py
@@ -1,14 +1,13 @@
 import streamlit as st
 from utils.database_connection import get_jobindsats_db
 import streamlit_antd_components as sac
-
+from graphs.aarshjul import aarshjul
 
 db_client = get_jobindsats_db()
 
 
 def get_politisk_betjening_overview():
     st.title("Politisk betjening")
-    st.subheader("Under udvikling")
 
     with st.sidebar:
         st.markdown("## Politisk betjening")
@@ -26,3 +25,9 @@ def get_politisk_betjening_overview():
             icon=None,
             color='#4a4a4a'
         )
+
+    if samling is None:
+        st.subheader("Vælg en underside i menuen til venstre")
+    else:
+        if samling == "Årshjulsdokumenter":
+            aarshjul()
