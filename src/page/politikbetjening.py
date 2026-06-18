855fd3507aa0575866227d55fef261398c9285bb
Nivethan Kandiah <nivethan.kandiah@randers.dk>
2026-06-18 09:02:39 +0200

    ensure to close db_connection

--- src/page/politikbetjening.py
+++ src/page/politikbetjening.py
@@ -7,27 +7,32 @@ db_client = get_jobindsats_db()
 
 
 def get_politisk_betjening_overview():
-    st.title("Politisk betjening")
+    try:
+        st.title("Politisk betjening")
 
-    with st.sidebar:
-        st.markdown("## Politisk betjening")
+        with st.sidebar:
+            st.markdown("## Politisk betjening")
 
-        samling = sac.tree(
-            items=[
-                sac.TreeItem("Årshjulsdokumenter"),
-                sac.TreeItem("Diverse statistikker")
-            ],
-            index=0,
-            size="lg",
-            open_all=False,
-            checkbox=False,
-            show_line=False,
-            icon=None,
-            color='#4a4a4a'
-        )
+            samling = sac.tree(
+                items=[
+                    sac.TreeItem("Årshjulsdokumenter"),
+                    sac.TreeItem("Diverse statistikker")
+                ],
+                index=0,
+                size="lg",
+                open_all=False,
+                checkbox=False,
+                show_line=False,
+                icon=None,
+                color='#4a4a4a'
+            )
 
-    if samling is None:
-        st.subheader("Vælg en underside i menuen til venstre")
-    else:
-        if samling == "Årshjulsdokumenter":
-            aarshjul()
+        if samling is None:
+            st.subheader("Vælg en underside i menuen til venstre")
+        else:
+            if samling == "Årshjulsdokumenter":
+                aarshjul()
+    except Exception as e:
+        st.error(f'Fejl ved visning af politisk betjening: {e}')
+    finally:
+        db_client.close_connection()
